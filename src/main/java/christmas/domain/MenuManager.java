6e683398a340b8228e6d18d158cd97a35d5c8229
HYOAN <aspqll@naver.com>
2023-11-13 17:58:28 +0900

    feat(christmas): 총주문 금액을 계산하는 기능 구현

--- src/main/java/christmas/domain/MenuManager.java
+++ src/main/java/christmas/domain/MenuManager.java
@@ -1,14 +1,16 @@
 package christmas.domain;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /*
- * 각 메뉴의 가격들을 저장하고 있다.
+ * 각 메뉴들의 가격을 저장하고 있다.
  * 사용자의 주문을 받아, 총주문 금액을 계산한다.
- * 10000원 이상일 경우 할인 이벤트가 적용된다.
  * */
 public class MenuManager {
-    private static final Map<String, Integer> MENU_ITEMS = Map.of(
+    private static final Map<String, Integer> FOOD_MENU = Map.of(
             "양송이수프", 6000,
             "타파스", 5500,
             "시저샐러드", 8000,
@@ -18,11 +20,29 @@ public class MenuManager {
             "크리스마스파스타", 25000,
             "초코케이크", 15000,
             "아이스크림", 5000
-            );
+    );
     private static final Map<String, Integer> DRINKS_MENU = Map.of(
             "제로콜라", 3000,
             "레드와인", 60000,
             "샴페인", 25000
     );
 
+    public int calculateTotalOrderCost(Map<String, Integer> orderList) {
+        List<String> menuNames = new ArrayList<>(orderList.keySet());
+        int total = 0;
+        int quantity = 0;
+        int price = 0;
+        for (String menuName : menuNames) {
+            price = getPrice(menuName);
+            quantity = orderList.get(menuName);
+            total += price * quantity;
+        }
+        return total;
+    }
+
+    public int getPrice(String menuName) {
+        if (FOOD_MENU.containsKey(menuName))
+            return FOOD_MENU.get(menuName);
+        return DRINKS_MENU.get(menuName);
+    }
 }
--- src/main/java/christmas/domain/OrderManager.java
+++ src/main/java/christmas/domain/OrderManager.java
@@ -1,26 +1,26 @@
 package christmas.domain;
 
+import java.awt.*;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 /* ChristmasPromotion
- * 사용자의 방문 날짜를 저장한다. <visitDate>
  * 입력 받은 주문을 정리하여 저장한다.
  * 음료만 주문할 수 없다.
  * 메뉴는 한 번에 최대 20개까지만 주문이 가능하다.
  * 할인 내역과 총주문 금액 등 주문에 대한 데이터들을 저장한다.
+ * 10000원 이상일 경우 할인 이벤트가 적용된다.
  * */
 public class OrderManager {
     private Map<String, Integer> orderList = new LinkedHashMap<>();
-    private int visitDate;
     private int totalOrderAmount;
     private int totalDiscountAmount;
 
     public void compileOrderList(String userOrder) {
-        String[] items = userOrder.split(",");
+        String[] orders = userOrder.split(",");
 
-        for (String item : items) {
-            String[] parts = item.split("-");
+        for (String order : orders) {
+            String[] parts = order.split("-");
             String menu = parts[0];
             int quantity = Integer.parseInt(parts[1]);
 
@@ -28,5 +28,9 @@ public class OrderManager {
         }
     }
 
+    public void calculateOrderPrices(MenuManager menuManager) {
+        int total = menuManager.calculateTotalOrderCost(orderList);
+        totalOrderAmount = total;
+    }
 
 }
