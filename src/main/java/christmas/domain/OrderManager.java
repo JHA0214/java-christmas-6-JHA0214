8715b6792636b501eb480e0d4d2d00ed7b156b28
HYOAN <aspqll@naver.com>
2023-11-14 23:32:35 +0900

    feat(christmas): 최종 결과를 출력해주는 기능 구현

--- src/main/java/christmas/domain/OrderManager.java
+++ src/main/java/christmas/domain/OrderManager.java
@@ -4,10 +4,30 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-/* ChristmasPromotion
+/* OrderManager
  * 입력 받은 주문을 정리하여 저장한다.
  * 할인 내역과 총주문 금액 등 주문에 대한 데이터들을 저장한다.
  * */
+enum CheckoutMessages {
+    ORDER_MENU("\n<주문 메뉴>"),
+    TOTAL_ORDER_AMOUNT("\n<할인 전 총주문 금액>"),
+    COMPLIMENTARY_ITEM("\n<증정 메뉴>"),
+    BENEFIT_DETAILS("\n<혜택 내역>"),
+    TOTAL_DISCOUNT_AMOUNT("\n<총혜택 금액>"),
+    EXPECTED_PAYMENT_AFTER_DISCOUNT("\n<할인 후 예상 결제 금액>"),
+    DECEMBER_EVENT_BADGE("\n<12월 이벤트 배지>");
+
+    private final String messages;
+
+    CheckoutMessages(String messages) {
+        this.messages = messages;
+    }
+
+    public String getmessages() {
+        return messages;
+    }
+}
+
 enum FoodType {
     APPETIZER, MAIN, DESSERT, DRINK
 }
@@ -18,7 +38,7 @@ public class OrderManager {
 
     private int totalOrderAmount;
     private int totalDiscountAmount;
-    private String eventBadge = "";
+    private String eventBadge = "없음";
 
     public OrderManager() {
     }
@@ -32,7 +52,7 @@ public class OrderManager {
         }
     }
 
-    public void compileOrderList(String userOrder) {
+    public Map<String, Integer> compileOrderList(String userOrder) {
         String[] orders = userOrder.split(",");
 
         for (String order : orders) {
@@ -42,6 +62,7 @@ public class OrderManager {
 
             orderList.put(menu, quantity);
         }
+        return orderList;
     }
 
     public void calculateOrderPrices(MenuManager menuManager) {
@@ -50,12 +71,16 @@ public class OrderManager {
         setOrderquantity(menuManager);
     }
 
+    public boolean checkEventEligibility() {
+        return totalOrderAmount > 10000;
+    }
+
     public void startDiscountProcess(PromotionManager promotionManager, DayType dayType, int visitDate) {
         totalDiscountAmount += promotionManager.christmasDiscount(visitDate);
         totalDiscountAmount += promotionManager.checkEligibleDiscounts(dayType, foodtypeQuantities);
         totalDiscountAmount += promotionManager.champagneGift(totalOrderAmount);
         if (totalDiscountAmount > 5000)
-            determineEventBadge();
+            eventBadge = determineEventBadge();
     }
 
     public String determineEventBadge() {
@@ -68,5 +93,23 @@ public class OrderManager {
         if (totalDiscountAmount > 15000)
             return "산타";
         return "트리";
+    }
+
+    public void printOrderResult(PromotionManager promotionManager) {
+        System.out.println(CheckoutMessages.ORDER_MENU.getmessages());
+        printOrderMenu();
+        System.out.println(CheckoutMessages.TOTAL_ORDER_AMOUNT.getmessages() + "\n" + totalOrderAmount);
+        System.out.println(CheckoutMessages.COMPLIMENTARY_ITEM.getmessages() + "\n" + promotionManager.checkChampagne());
+        System.out.println(CheckoutMessages.BENEFIT_DETAILS.getmessages());
+        promotionManager.printDiscounthistory(totalOrderAmount);
+        System.out.println(CheckoutMessages.TOTAL_DISCOUNT_AMOUNT.getmessages() + "\n" + totalDiscountAmount * -1);
+        System.out.println(CheckoutMessages.EXPECTED_PAYMENT_AFTER_DISCOUNT.getmessages() + "\n" + (totalOrderAmount - totalDiscountAmount));
+        System.out.println(CheckoutMessages.DECEMBER_EVENT_BADGE.getmessages() + "\n" + eventBadge);
+    }
+
+    public void printOrderMenu() {
+        for (Map.Entry<String, Integer> entry : orderList.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue() + "개");
+        }
     }
 }
